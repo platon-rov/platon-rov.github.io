3d8b78d9a0202ba28ba0dd216f2ab89f017d30a4
Platon Rov <platon.rov@gmail.com>
2019-11-27 19:26:26 +0200

    crypta deleted

--- crypta/js/pr.common.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-     $('.toggle-mob-slide-items').click(item => {
-          $('.toggle-mob-slide-items').fadeOut();
-          $('.slide__item-mob-collapsed').removeClass('slide__item-mob-collapsed');
-     })
-});
